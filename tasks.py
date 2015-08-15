03b20cdcd878c460cc131f8d7e4c61f4d8860fa7
Jimmy Thrasibule <jimmy.thrasibule@gmail.com>
2015-08-16 00:15:59 +0200

    tasks.py: Add filesystem utilities
    Add the ``fs`` class to manipulate the filesystem. The ``fs`` class
    contains high level file operation functions to simplify tasks
    definitions.
    
    fs.copytree(src, dst)
        Copy the directory structure from *src* to replicate it
        in *dst*. Only the directory structure is copied not the files.
    
    fs.lstree(path, recursive=False, include_path=False)
        Return a list with the content of given directory.
    
    fs.rmdir(path, recursive=False)
        Remove empty directories.
    
    fs.rmtree(pattern)
        Remove an entire directory tree matching given shell pattern.
    
    fs.shexpand(pattern)
        Expand shell-like path name patterns.
    
    fs.symlink(source, link_name, force=False, target_is_directory=False)
        Create a symbolic link named *link_name* pointing to *source*.
    
    Signed-off-by: Jimmy Thrasibule <jimmy.thrasibule@gmail.com>

--- tasks.py
+++ tasks.py
@@ -23,7 +23,12 @@
 
 import re
 
+import os
 import sys
+import glob
+import shutil
+
+from contextlib import suppress
 
 from invoke import Collection
 
@@ -211,4 +216,191 @@ class msg(object):
             max_try -= 1
 
         return None
+
+
+#
+# File system
+# ^^^^^^^^^^^
+
+class fs(object):
+    """Namespace for filesystem related operations."""
+
+    @staticmethod
+    def shexpand(pattern):
+        """Return a possibly-empty list of path names that match
+        *pattern*.
+
+        :param pattern: A string or a list of strings containing
+                        shell-style wildcards.
+        :type pattern: str or iterable
+
+        :returns: A list of path names matching given *pattern*.
+        :rtype: list
+
+        """
+        if isinstance(pattern, (str, bytes)):
+            it = [pattern, ]
+        else:
+            it = pattern
+
+        def _expand(x):
+            return glob.glob(os.path.expanduser(os.path.expandvars(x)))
+
+        # Flatten list of paths from _expand.
+        return [
+            item
+            for paths_list in map(_expand, it)
+            for item in paths_list
+        ]
+
+
+    @staticmethod
+    def copytree(src, dst):
+        """Copy the directory tree structure from *src* to recreate it
+        in the *dst* directory.
+
+        :param str src: Path to the source to replicate the directory
+                        tree structure from.
+
+        :param str dst: Path to the destination directory to replicate
+                        the directory tree structure.
+
+        """
+        os.makedirs(dst, exist_ok=True)
+        for root, dirs, _ in os.walk(src):
+            for d in dirs:
+                os.makedirs(
+                    os.path.join(root.replace(src, dst), d),
+                    exist_ok=True
+                )
+
+
+    @classmethod
+    def lstree(cls, pattern, recursive=False, include_path=False):
+        """List all files and directories found in given path.
+
+        :param iterable pattern: A string or list of strings containing
+                                 shell-style patterns of directories to
+                                 remove.
+
+        :param bool recursive: Should the content of any directory
+                               found in ``path`` also be listed?
+                               Default to **False**.
+
+        :param bool include_path: Should given ``path`` also be included
+                                  in returned list.
+
+        :returns: A list of items found in given path.
+        :rtype: list
+
+        """
+        lst = []
+        for path in cls.shexpand(pattern):
+            for root, dirs, files in os.walk(path):
+                if root == path and include_path:
+                    lst.append(root)
+
+                lst += map(lambda x: os.path.join(root, x), dirs)
+                lst += map(lambda x: os.path.join(root, x), files)
+
+                if not recursive:
+                    break
+
+        return lst
+
+
+    @classmethod
+    def rmdir(cls, pattern, recursive=False):
+        """Remove empty directory at *path*. When recursive is set to
+        ``True``, run over the directory tree to remove any empty
+        directory found.
+
+        :param iterable pattern: A string or list of strings containing
+                                 shell-style patterns of directories to
+                                 remove.
+
+        :param bool recursive: Should *path* be walked through to remove
+                               empty directories found in the tree?
+
+        """
+        for path in sorted(cls.shexpand(pattern)):
+            if recursive:
+                # Remove empty directories in tree from deepest to
+                # shallowest.
+                for root, dirs, _ in os.walk(path, topdown=False):
+                    for d in dirs:
+                        with suppress(OSError):
+                            os.rmdir(os.path.join(root, d))
+
+            with suppress(OSError):
+                os.rmdir(path)
+
+
+    @classmethod
+    def rmtree(cls, pattern):
+        """Remove directory trees matching given Unix shell style
+        patterns.
+
+        :param iterable pattern: A string or list of strings containing
+                                 shell-style patterns of directories to
+                                 remove.
+
+        """
+        for path in sorted(cls.shexpand(pattern)):
+            # Avoid two removal tries if path does not exist.
+            if not os.path.lexists(path):
+                continue
+
+            try:
+                # Try to remove path (and sub-paths) as a directory.
+                shutil.rmtree(path)
+            except OSError:
+                # Not a directory, try to remove path as a file.
+                with suppress(OSError):
+                    os.remove(path)
+
+
+    @classmethod
+    def symlink(cls, source, link_name, force=False, target_is_directory=False):
+        """Create a symbolic link pointing to *source* named *link_name*.
+
+        :param str source: Path of the target to link to.
+        :param str link_name: Path to the symbolic link to create.
+        :param bool force: If set to ``True`` if *link_name* exists, it
+                           will be removed before creating the link.
+                           Default to ``False``.
+        :param bool target_is_directory:
+          On Windows, a symlink represents either a file or a directory
+          and does not morph to the target dynamically. Symlink will be
+          created as a directory if set to ``True``. Default to
+          ``False``.
+
+        :returns: ``True`` if symlink creation suceeded, ``False``
+                  otherwise.
+        :rtype: bool
+
+        """
+        with suppress(OSError):
+            if os.readlink(link_name) == source \
+               or os.path.abspath(os.readlink(link_name)) == source:
+                return True
+
+        if force:
+            if os.path.lexists(link_name):
+                cls.rmtree(link_name)
+            else:
+                try:
+                    os.makedirs(link_name, exist_ok=True)
+                except OSError:
+                    return False
+                cls.rmdir(link_name)
+
+        try:
+            os.symlink(
+                source, link_name, target_is_directory=target_is_directory
+            )
+        except (NotImplementedError, OSError):
+            return False
+
+        return True
 
